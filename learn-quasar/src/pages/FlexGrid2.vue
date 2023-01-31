708d9ade68155f0e8833c3d0706243313e7a41ee
이세진 <sedin@kakao.com>
2023-01-31 23:23:19 +0900

    feat: Flexbox Grid 기능 구현

---
+++ learn-quasar/src/pages/FlexGrid2.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="q-pa-xl">
+    <section class="q-mb-xl">
+      <div class="text-h4">Flex Grid 2</div>
+    </section>
+  </q-page>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- learn-quasar/src/router/routes.js
+++ learn-quasar/src/router/routes.js
@@ -27,6 +27,14 @@ const routes = [
         path: 'classes-variables',
         component: () => import('src/pages/ClassesVariables.vue'),
       },
+      {
+        path: 'flex-grid-1',
+        component: () => import('src/pages/FlexGrid1.vue'),
+      },
+      {
+        path: 'flex-grid-2',
+        component: () => import('src/pages/FlexGrid2.vue'),
+      },
     ],
   },
   {
