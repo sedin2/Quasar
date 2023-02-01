e03452e51a52e282f25f132609e2793ec8b53a09
이세진 <sedin@kakao.com>
2023-02-01 23:57:46 +0900

    feat: instagram profile 클론 뼈대 작성

--- learn-quasar/src/pages/FlexGrid2.vue
+++ learn-quasar/src/pages/FlexGrid2.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="q-pa-xl">
     <section class="q-mb-xl">
-      <div class="text-h4">Flex Grid 2</div>
+      <div class="text-h4">Instagram Profile</div>
     </section>
   </q-page>
 </template>
