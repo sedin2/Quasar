2d0c6b785627e74dbdae67fd11c6d2ee304e9e23
이세진 <sedin@kakao.com>
2023-02-02 23:47:40 +0900

    feat: instagram clone

--- learn-quasar/src/pages/FlexGrid2.vue
+++ learn-quasar/src/pages/FlexGrid2.vue
@@ -1,11 +1,37 @@
 <template>
-  <q-page class="q-pa-xl">
-    <section class="q-mb-xl">
-      <div class="text-h4">Instagram Profile</div>
+  <q-page padding>
+    <section class="bg-red row">
+      <div class="bg-primary">
+        <q-avatar size="150px">
+          <q-img src="/favicon.ico" />
+        </q-avatar>
+      </div>
+      <div class="bg-secondary">
+        프로필 정보
+        <div class="bg-red">
+          <span>sedin_2</span>
+          <q-btn label="프로필 편집" />
+          <q-btn label="설정 버튼" />
+        </div>
+        <div class="bg-green">
+          <span>게시물 0</span>
+          <span>팔로워 53</span>
+          <span>팔로우 76</span>
+        </div>
+        <div class="bg-blue">
+          <div>SEJIN</div>
+        </div>
+      </div>
     </section>
+    <section class="bg-yellow"></section>
+    <section class="bg-green"></section>
   </q-page>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.q-page {
+  background-color: #fafafa;
+}
+</style>
