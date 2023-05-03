d4e3c23519fd4adc824e1e19e3f1220dd64c62de
sedin2 <sedin@kakao.com>
2023-05-03 23:16:24 +0900

    feat: 로그인 폼 컴포넌트 작성

--- learn-quasar/src/pages/auth/SignIn.vue
+++ learn-quasar/src/pages/auth/SignIn.vue
@@ -1,6 +1,22 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="text-h6">SignIn</div>
+    <q-card flat :style="{minWidth: '400px'}">
+      <q-card-section>
+        <div class="text-h6 text-weight-bold">로그인 하기</div>
+        <div class="text-sub-tilte2 text-grey">방문 해 주셔서 감사합니다.</div>
+      </q-card-section>
+      <q-card-section>
+        <q-form>
+          <div class="q-gutter-y-lg">
+            <q-input filled label="이메일" hint="example@email.com"></q-input>
+            <q-input filled label="비밀번호" hint="영문 대/소문자 포함 8자 이상"></q-input>
+          </div>
+          <q-btn class="q-mt-lg full-width" unelevated color="primary" size="lg" label="로그인 하기" />
+          <q-btn class="q-mt-md full-width" unelevated color="primary" size="lg" label="회원가입" to="/auth/sign-up" />
+          <q-btn class="q-mt-sm full-width" unelevated color="primary" size="lg" label="홈으로" to="/" />
+        </q-form>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
