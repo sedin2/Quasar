8463a257180219a43786657f05125357516d95d2
sedin2 <sedin@kakao.com>
2023-05-04 20:49:31 +0900

    feat: 회원가입 컴포넌트 작성

--- learn-quasar/src/pages/auth/SignIn.vue
+++ learn-quasar/src/pages/auth/SignIn.vue
@@ -1,23 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card flat :style="{minWidth: '400px'}">
-      <q-card-section>
-        <div class="text-h6 text-weight-bold">로그인 하기</div>
-        <div class="text-sub-tilte2 text-grey">방문 해 주셔서 감사합니다.</div>
-      </q-card-section>
-      <q-card-section>
-        <q-form>
-          <div class="q-gutter-y-lg">
-            <q-input filled label="이메일" hint="example@email.com"></q-input>
-            <q-input filled label="비밀번호" hint="영문 대/소문자 포함 8자 이상"></q-input>
-          </div>
-          <q-btn class="q-mt-lg full-width" unelevated color="primary" size="lg" label="로그인 하기" />
-          <q-btn class="q-mt-md full-width" unelevated color="primary" size="lg" label="회원가입" to="/auth/sign-up" />
-          <q-btn class="q-mt-sm full-width" unelevated color="primary" size="lg" label="홈으로" to="/" />
-        </q-form>
-      </q-card-section>
-    </q-card>
+  <!-- class="flex flex-center" -->
+  <q-page>
+    <div class="row window-height">
+      <div class="col" :class="leftColStyle">
+        <q-img
+          height="100vh"
+          src="/sunflower.jpg"
+        />
+      </div>
+      <div class="col flex flex-center">
+        <q-card flat :style="{minWidth: '400px'}">
+          <q-card-section>
+            <div class="text-h6 text-weight-bold">로그인 하기</div>
+            <div class="text-sub-tilte2 text-grey">방문 해 주셔서 감사합니다.</div>
+          </q-card-section>
+          <q-card-section>
+            <q-form>
+              <div class="q-gutter-y-lg">
+                <q-input filled label="이메일" hint="example@email.com"></q-input>
+                <q-input filled type="password" label="비밀번호" hint="영문 대/소문자 포함 8자 이상"></q-input>
+              </div>
+              <q-btn class="q-mt-lg full-width" unelevated color="primary" size="lg" label="로그인 하기" />
+              <q-btn class="q-mt-md full-width" unelevated color="primary" size="lg" label="회원가입" to="/auth/sign-up" />
+              <q-btn class="q-mt-sm full-width" unelevated color="primary" size="lg" label="홈으로" to="/" />
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+    
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar();
+const leftColStyle = computed(() => ({hidden: $q.screen.lt.md}));
+</script>
--- learn-quasar/src/pages/auth/SignUp.vue
+++ learn-quasar/src/pages/auth/SignUp.vue
@@ -1,6 +1,22 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="text-h6">SignUp</div>
+    <q-card flat :style="{minWidth: '400px'}">
+      <q-card-section>
+        <div class="text-h6 text-weight-bold">회원 가입</div>
+        <div class="text-sub-tilte2 text-grey">감사합니다.</div>
+      </q-card-section>
+      <q-card-section>
+        <q-form>
+          <div class="q-gutter-y-lg">
+            <q-input filled label="이메일" hint="example@email.com"></q-input>
+            <q-input filled type="password" label="비밀번호" hint="영문 대/소문자 포함 8자 이상"></q-input>
+            <q-input filled type="password" label="비밀번호 확인" hint=""></q-input>
+          </div>
+          <q-btn class="q-mt-lg full-width" unelevated color="primary" size="lg" label="가입 하기" />
+          <q-btn class="q-mt-md full-width" unelevated color="primary" size="lg" label="로그인 하기" to="/auth/sign-in" />
+        </q-form>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
