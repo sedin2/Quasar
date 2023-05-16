c4edb8c6d8a1c56194ddc7764a9385e8886a58ac
sedin2 <sedin@kakao.com>
2023-05-16 18:52:48 +0900

    feat: date util 작성

--- learn-quasar/src/pages/QuasarUtils.vue
+++ learn-quasar/src/pages/QuasarUtils.vue
@@ -3,10 +3,24 @@
     <section class="q-mb-xl">
       <div class="text-h4">Date Utils</div>
       <q-separator class="q-my-md" />
+      <div class="text-h6">
+        formatDate - {{ formatDate(new Date(), FORMAT) }}
+      </div>
+      <div class="text-h6">
+        addToDate - {{ formatDate(addToDate(new Date(), { days: 7 }), FORMAT) }}
+      </div>
+      <div class="text-h6">
+        subtractFromDate - {{ formatDate(subtractFromDate(new Date(), { days: 7 }), FORMAT) }}
+      </div>
     </section>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import { date } from 'quasar';
+
+const FORMAT = 'YYYY-MM-DD HH:mm:ss';
+const { formatDate, addToDate, subtractFromDate } = date;
+</script>
 
 <style lang="scss" scoped></style>
