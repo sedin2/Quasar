4a5b1c29c9abf1745275cf9d0c350d6ab1732a77
sedin2 <sedin@kakao.com>
2023-05-16 18:59:15 +0900

    feat: type checking util 작성

--- learn-quasar/src/pages/QuasarUtils.vue
+++ learn-quasar/src/pages/QuasarUtils.vue
@@ -13,14 +13,28 @@
         subtractFromDate - {{ formatDate(subtractFromDate(new Date(), { days: 7 }), FORMAT) }}
       </div>
     </section>
+    <section class="q-mb-xl">
+      <div class="text-h4">Type Checking Utils</div>
+      <q-separator class="q-my-md" />
+      <div class="text-h6">
+        origin checking - {{ objA == objB }}
+      </div>
+      <div class="text-h6">
+        type checking utils - {{ deepEqual(objA, objB) }}
+      </div>
+    </section>
   </q-page>
 </template>
 
 <script setup>
-import { date } from 'quasar';
+import { date, is } from 'quasar';
 
 const FORMAT = 'YYYY-MM-DD HH:mm:ss';
 const { formatDate, addToDate, subtractFromDate } = date;
+const { deepEqual } = is;
+
+const objA = { name: 'abc', age: 10 }
+const objB = { name: 'abc', age: 10 }
 </script>
 
 <style lang="scss" scoped></style>
